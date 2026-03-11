d65b752d3937e539c1898966ce10e088efa24f12
Francisco Hernández <fran.hrdz93@gmail.com>
2026-03-10 21:34:00 -0600

    Completando vista móvil

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: './', // <-- AÑADE ESTA LÍNEA EXACTA AQUÍ
   plugins: [
     react(),
     tailwindcss(),
